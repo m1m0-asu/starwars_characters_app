69cc5ba8d7560212e1ba2b4070aba42387e0d52d
m1m0-asu <mmadhu1@asu.edu>
2022-12-07 14:43:50 -0700

    inital commit

--- react-backend/client/src/App.js
+++ react-backend/client/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 import React, {useEffect, useState} from 'react';
 
 function App() {
 
-  const [users, setUsers] = useState([])
+  const [people, setPeople] = useState([])
 
   useEffect (() => {
     console.log('useeffect run')
     fetch('/people')
       .then(res => res.json())
-      .then(users => setUsers(users))
+      .then(people => setPeople(people))
   },[]);
 
   return (
     <div className="App">
-      <h1>Users</h1>
-      {users.map(user =>
-        <div key={user.id}>{user.username}</div>
+      <h1>People</h1>
+      {people.map(peep => <div>
+        <div key={peep.name}>{peep.name}</div>
+        <div key={peep.gender}>{peep.gender}</div>
+        </div>
       )}
     </div>
   );
--- react-backend/routes/people.js
+++ react-backend/routes/people.js
@@ -4,16 +4,38 @@ var router = express.Router();
 
 /* GET people listing. */
 router.get('/', function(req, res, next) {
-    getPeople().then(people => {
-        console.log(people)
-        res.json(people)
-    })
+    getNFirstFemaleCharacters(5)
+        .then(people => {
+            res.json(people)
+        })
+        .catch(err => {
+            res.json({error: "Something went wrong"})
+        })
 });
 
-async function getPeople() {
-    const url = "https://swapi.dev/api/people/";
+async function getPeople(url) {
+    url = url ?? "https://swapi.dev/api/people/";
     const data = await got(url).json();
     return data;
+}
+
+async function getNFirstFemaleCharacters(count) {
+    const url = "https://swapi.dev/api/people/?page=";
+    let people = [];
+    for (let pageNumber=1; ;pageNumber++) {
+        let data = await getPeople(url + pageNumber);
+        for (let peep of data.results) {
+            if (people.length < 5) {
+                if (peep.gender === 'female') {
+                    people.push(peep);
+                }
+            }
+            else {
+                return people;
+            }
+        }
+    }
+    return people;
 }
 
 module.exports = router;
